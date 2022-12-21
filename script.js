43f2f3aff772a6cc51857d46aee067303eca6c1e
George F <78384472+Wozunubi@users.noreply.github.com>
2022-12-21 01:13:09 -0500

    planned structure

---
+++ script.js
@@ -0,0 +1,68 @@
+function selectionSort() {
+
+}
+
+function insertionSort() {
+
+}
+
+function quickSort() {
+
+}
+
+function mergeSort() {
+
+}
+
+function heapSort() {
+
+}
+
+function radixSort() {
+
+}
+
+function shellSort() {
+
+}
+
+function bucketSort() {
+
+}
+
+function countingSort() {
+    
+}
+
+function bubbleSort() {
+
+}
+
+function bogoSort() {
+
+}
+
+function draw() {
+
+}
+
+function simulate() {
+
+}
+
+function scramble() {
+
+}
+
+function settings() {
+    //Something
+    update();
+}
+
+function update() {
+    simulate();
+    draw();
+    requestAnimationFrame(update);
+}
+
+settings();

d7a0708feaeed0b71c4122a10a03026a36b46b3c
George F <78384472+Wozunubi@users.noreply.github.com>
2022-12-24 17:39:35 -0500

    animation implemented

--- script.js
+++ script.js
@@ -4,8 +4,60 @@ var c = canvas.getContext("2d");
 canvas.width = window.innerWidth - 20;
 canvas.height = window.innerHeight - 100;
 
-function bogoSort() {
+var items = [];
+var method = "";
+var speed = 60;
+var x = 200;
 
+function isSorted(list) {
+    for (var i = 1; i < list.length; i++) {
+        if (list[i].val < list[i-1].val) return false;
+    }
+    return true;
+}
+
+function draw() {
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.beginPath();
+
+    for (var i = 0; i < items.length; i++) {
+        c.rect((i*items[i].width)+20, canvas.height-items[i].height-20, items[i].width, items[i].height);
+        c.fillStyle = items[i].colour;
+        c.fill();
+        c.stroke();
+
+        //if (items[i].colour == "#FF0000") items[i].colour = "#808080";
+    }
+}
+
+function simulate() {
+    bogoSort();
+}
+
+function scramble() {
+    items = [];
+    itemNum = parseInt(document.getElementById("itemNum").value);
+    for (var i = 1; i <= itemNum; i++) {
+        items.push({val: i, 
+            width: (canvas.width-40)/itemNum, 
+            height: (i*(canvas.height-40))/itemNum, 
+            colour: "#808080"});
+    }
+    items.sort(() => Math.random() - 0.5);
+    method = document.getElementById("method");
+    update();
+}
+
+function update() {
+    simulate();
+    draw();
+    requestAnimationFrame(update);
+}
+
+function bogoSort() {
+    if (!isSorted(items)) {
+        items.sort(() => Math.random() - 0.5);
+    }
 }
 
 function bubbleSort() {
@@ -47,28 +99,3 @@ function selectionSort() {
 function shellSort() {
 
 }
-
-function draw() {
-
-}
-
-function simulate() {
-
-}
-
-function reset() {
-
-}
-
-function settings() {
-    //Something
-    update();
-}
-
-function update() {
-    simulate();
-    draw();
-    requestAnimationFrame(update);
-}
-
-settings();
